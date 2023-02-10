06511587ab40d03c80dee762781efe5a4b31a648
caioguedesz <caioamaralguedes@gmail.com>
2023-02-10 11:50:41 -0300

    concluded code

--- Cores - condicionantes.py
+++ Cores - condicionantes.py
@@ -5,5 +5,9 @@ if (cor1 == cor2 ):
  print('A cor resultante dessa mistura é ' + cor1)
 elif (cor1 == 'vermelho' and cor2 == 'azul' or cor1 == 'azul' and cor2 == 'vermelho'):
     print('A cor resultante dessa mistura é magenta')
-
+elif  (cor1 =='vermelho' and cor2 == 'verde' or cor1 == 'verde' and cor2 == 'vermelho'):
+    print('A cor resultante dessa mistura é amarelo')
+elif (cor1 == 'verde' and cor2 == 'azul' or cor1 == 'azul' and cor2 == 'verde'):  
+    print('A cor resultante dessa mistura é Ciano')
  
+#concluded the other colors for training
