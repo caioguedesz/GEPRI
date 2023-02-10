f3bbeb42a71194756752a58c4cecfa599b72fb53
caioguedesz <caioamaralguedes@gmail.com>
2023-02-09 21:34:21 -0300

    First test

---
+++ Condicionantes.py
@@ -0,0 +1,3 @@
+idade = int(input('Quantos anos você tem? '))
+divida_eleitoral = True or False
+print(f'Você pode ter divida eleitoral? {idade>=18 and idade<70}')
---
+++ Cores - condicionantes.py
@@ -0,0 +1,9 @@
+cor1 = str(input('Digite a primeira cor: '))
+cor2 = str(input('Digite a segunda cor: '))
+
+if (cor1 == cor2 ):
+ print('A cor resultante dessa mistura é ' + cor1)
+elif (cor1 == 'vermelho' and cor2 == 'azul' or cor1 == 'azul' and cor2 == 'vermelho'):
+    print('A cor resultante dessa mistura é magenta')
+
+ 
